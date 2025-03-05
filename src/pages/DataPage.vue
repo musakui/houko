4e5f50e5e7f65ead5622d74239ae46c71b52e900
randName <kwokshungit@gmail.com>
2025-03-05 23:38:37 +0800

    datap

---
+++ src/pages/DataPage.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { useStore } from '../utils/store.js'
+const props = defineProps({ name: String })
+const { store } = useStore()
+const config = store.configs?.[props.name]
+</script>
+
+<template>
+	<div v-if="config">
+		{{ config }}
+	</div>
+	<div v-else>
+		no config for {{ props.name }}
+	</div>
+</template>
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,12 @@
+/** @satisfies {import('vue-router').RouteRecordRaw[]} */
 export const routes = [
 	{ path: '/', name: 'home', component: () => import('./pages/HomePage.vue') },
+	{
+		name: 'data',
+		path: '/d/:name',
+		props: true,
+		component: () => import('./pages/DataPage.vue'),
+	},
 	{
 		name: 'notFound',
 		path: '/:match(.*)',
